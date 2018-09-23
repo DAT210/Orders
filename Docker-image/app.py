cb3850cbf36aa6a7c26a9c743c351686fd38109f
Thomas <thomas.osaland@gmail.com>
2018-09-23 18:30:26 +0200

    Added docker-compose file and new py-server

--- Docker-image/app.py
+++ Docker-image/app.py
@@ -1,24 +1,21 @@
-from flask import Flask
-from redis import Redis, RedisError
-import os
-import socket
+from flask import Flask, request, render_template
+import mysql.connector
+import re
+
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
-
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
+
+
+@app.route("/")
+def index():
+    """Index page that shows a list of properties"""
+
+    return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
