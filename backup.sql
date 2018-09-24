fbffa4ce657f7830050b35f8a0c9394500eff960
Jacob Ihle <jacob.ihle@gmail.com>
2018-09-24 17:09:01 +0200

    updated backup dump

--- backup.sql
+++ backup.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.12, for Linux (x86_64)
 --
--- Host: localhost    Database: orderstest
+-- Host: localhost    Database: Orders
 -- ------------------------------------------------------
 -- Server version	8.0.12
 
@@ -16,26 +16,109 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `orders`
+-- Table structure for table `Customer`
 --
 
-DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `Customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
  SET character_set_client = utf8mb4 ;
-CREATE TABLE `orders` (
-  `OrderID` int(11) NOT NULL,
-  `time` int(11) DEFAULT NULL,
-  PRIMARY KEY (`OrderID`)
+CREATE TABLE `Customer` (
+  `Customer_ID` int(11) NOT NULL,
+  `Firstname` varchar(32) DEFAULT NULL,
+  `Lastname` varchar(32) DEFAULT NULL,
+  `TLF` int(11) DEFAULT NULL,
+  `MAIL` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`Customer_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `orders`
+-- Dumping data for table `Customer`
 --
 
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+LOCK TABLES `Customer` WRITE;
+/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Orders`
+--
+
+DROP TABLE IF EXISTS `Orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `Orders` (
+  `Order_ID` int(11) NOT NULL,
+  `Product_ID` int(11) DEFAULT NULL,
+  `Order_Time` datetime DEFAULT NULL,
+  `Payment` varchar(12) DEFAULT NULL,
+  `Delivery` varchar(32) DEFAULT NULL,
+  `Price` int(11) DEFAULT NULL,
+  `Betalt` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`Order_ID`),
+  CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`Order_ID`) REFERENCES `Product` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Orders`
+--
+
+LOCK TABLES `Orders` WRITE;
+/*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Orders_Cust`
+--
+
+DROP TABLE IF EXISTS `Orders_Cust`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `Orders_Cust` (
+  `Order_ID` int(11) DEFAULT NULL,
+  `Customer_ID` int(11) DEFAULT NULL,
+  KEY `Order_ID` (`Order_ID`),
+  KEY `Customer_ID` (`Customer_ID`),
+  CONSTRAINT `Orders_Cust_ibfk_1` FOREIGN KEY (`Order_ID`) REFERENCES `Orders` (`order_id`),
+  CONSTRAINT `Orders_Cust_ibfk_2` FOREIGN KEY (`Customer_ID`) REFERENCES `Customer` (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Orders_Cust`
+--
+
+LOCK TABLES `Orders_Cust` WRITE;
+/*!40000 ALTER TABLE `Orders_Cust` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Orders_Cust` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Product`
+--
+
+DROP TABLE IF EXISTS `Product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `Product` (
+  `Product_ID` int(11) NOT NULL,
+  `sauce` int(11) DEFAULT NULL,
+  `cheese` int(11) DEFAULT NULL,
+  `pepperoni` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Product_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Product`
+--
+
+LOCK TABLES `Product` WRITE;
+/*!40000 ALTER TABLE `Product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -47,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-24 13:37:34
+-- Dump completed on 2018-09-24 15:08:37
