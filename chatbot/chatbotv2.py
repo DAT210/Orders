7ea592c94c281ff93ddf388e02c791b7378d56c0
Vilde Riveland <vilderiveland@gmail.com>
2018-10-14 00:40:34 +0200

    better structure in spacy chatbot

--- chatbot/chatbotv2.py
+++ chatbot/chatbotv2.py
@@ -2,31 +2,53 @@ import spacy
 import spacy.matcher
 from spacy.matcher import Matcher
 from spacy.matcher import PhraseMatcher
+
 nlp = spacy.load('en_core_web_sm')
+nlp_sent = ""
 sent = ""
 openingTimesList = ['opening', 'open']
 matcher = PhraseMatcher(nlp.vocab)
+
+response = ""
 
 
 def init():
     patterns = [nlp(text) for text in openingTimesList]
     matcher.add('openingTimes', None, *patterns)
-    testing()
-    get_match_id("opening? times thursdays")
 
 
-def get_match_id(sentence):
+def respond(sentence):
+    prep_sentence(sentence)
+    matches = get_match_id()
+    for match in matches:
+        sort(match)
+
+
+def prep_sentence(sentence):
     global sent
     sent = sentence
-    sentence = nlp(sentence)
-    matches = matcher(sentence)
+    nlpsent = nlp(sent)
+    global nlp_sent
+    nlp_sent = nlpsent
+    # lemma before saving as nlp_sent
+
+
+def get_match_id():
+    matches = matcher(nlp_sent)
+    allmatches = []
     for match_id, start, end in matches:
-        sort(match_id, start, end)
+        match = {
+            'id': match_id,
+            'start': start,
+            'end': end
+        }
+        allmatches.append(match)
+    return allmatches
 
 
-def sort(id, start, end):
-    print(id)
-    if id == 1715934218517773148:
+def sort(match):
+    print(match['id'])
+    if match['id'] == 1715934218517773148:
         print('openingTimes')
 
 
@@ -42,3 +64,4 @@ def testing():
 
 
 init()
+respond("opening")
