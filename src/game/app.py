b2951235d03806c2cabaa2f62df39cc417443f8c
durdal <urdal.daniel@gmail.com>
2018-10-16 12:27:56 +0200

    Changed player and food into classes

--- src/game/app.py
+++ src/game/app.py
@@ -12,15 +12,12 @@ black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
 ball = pygame.image.load("images/intro_ball.gif")
-ballrect = ball.get_rect()
-food = pygame.image.load("images/food.png")
-foodrect = food.get_rect()
+foodimage = pygame.image.load("images/food.png")
 obstacle = pygame.image.load("images/big_obstacle.png")
 
-playerspeed = [0, 0]
-obstspeed = [1, 1]
-foodrect.topright = (width, 0)
-foodspeed = [-1, -1]
+player = Object(ball.get_rect(), [0, 0])
+food = Object(foodimage.get_rect(), [-1, -1])
+food.rect.topright = (width, 0)
 
 obstacles = {
     "center": Object(obstacle.get_rect(), [0, 0]),
@@ -41,15 +38,15 @@ score = 0
 print("Game started!")
 
 # setspeed sets speed based on arrow keys pressed
-def setspeed(speed):
-    if pygame.key.get_pressed()[pygame.K_UP] and ballrect.top > 0:
-        speed[1] = -2
-    if pygame.key.get_pressed()[pygame.K_DOWN] and ballrect.bottom < height:
-        speed[1] = 2
-    if pygame.key.get_pressed()[pygame.K_LEFT] and ballrect.left > 0:
-        speed[0] = -2
-    if pygame.key.get_pressed()[pygame.K_RIGHT] and ballrect.right < width:
-        speed[0] = 2
+def setplayerspeed():
+    if pygame.key.get_pressed()[pygame.K_UP] and player.rect.top > 0:
+        player.speed[1] = -2
+    if pygame.key.get_pressed()[pygame.K_DOWN] and player.rect.bottom < height:
+        player.speed[1] = 2
+    if pygame.key.get_pressed()[pygame.K_LEFT] and player.rect.left > 0:
+        player.speed[0] = -2
+    if pygame.key.get_pressed()[pygame.K_RIGHT] and player.rect.right < width:
+        player.speed[0] = 2
     return
 
 # boundce reverses the speed of a rect if it touches the borders
@@ -63,16 +60,16 @@ def bounce(rect, speed):
 # foodstart checks where the player is and finds an appropriate starting point for food
 def foodstart():
     xcoord, ycoord = 10, 10
-    if ballrect.center[0] < (width / 2):
+    if player.rect.center[0] < (width / 2):
         xcoord = width-10
-    if ballrect.center[1] < (height / 2):
+    if player.rect.center[1] < (height / 2):
         ycoord = height-10
     return xcoord, ycoord
 
 # iscollision checks if the player is colliding with an obstacle
 def iscollision():
     for key in obstacles:
-        if ballrect.colliderect(obstacles[key].rect):
+        if player.rect.colliderect(obstacles[key].rect):
             return True
     return False
 
@@ -96,29 +93,29 @@ while 1:
         if event.type == pygame.QUIT:
             quit()
 
-    playerspeed = [0, 0]
-    setspeed(playerspeed)
-    ballrect = ballrect.move(playerspeed)
+    player.speed = [0, 0]
+    setplayerspeed()
 
-    if ballrect.colliderect(foodrect):
+    player.rect = player.rect.move(player.speed)
+
+    if player.rect.colliderect(food.rect):
         score += 100
         xcoord, ycoord = foodstart()
-        foodrect.center = (xcoord, ycoord)
-        increasespeed(foodspeed)
+        food.rect.center = (xcoord, ycoord)
+        increasespeed(food.speed)
 
     if iscollision():
         quit()
 
-    bounce(foodrect, foodspeed)
-    foodrect = foodrect.move(foodspeed)
+    bounce(food.rect, food.speed)
+    food.rect = food.rect.move(food.speed)
 
-
-
-    screen.fill(black)
+    screen.fill(black)    
     for key in obstacles:
         obstacles[key].rect = obstacles[key].rect.move(obstacles[key].speed)
         bounce(obstacles[key].rect, obstacles[key].speed)
         screen.blit(obstacle, obstacles[key].rect)
-    screen.blit(food, foodrect)
-    screen.blit(ball, ballrect)
+    
+    screen.blit(foodimage, food.rect)
+    screen.blit(ball, player.rect)
     pygame.display.flip()
