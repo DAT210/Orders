387861d87f1e531cb6feadd265049f2d521e843b
Vilde Riveland <vilderiveland@gmail.com>
2018-10-23 20:26:11 +0200

    small change to script and html template

--- chatbot/static/script.js
+++ chatbot/static/script.js
@@ -1,3 +1,10 @@
-function init(){
-    alert("init template works");
+$( document ).ready(function() {
+
+});
+
+function ask(){
+    var question = document.forms["input"]["question"].value;
+
+
+    return false;
 }
