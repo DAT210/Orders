a9bbd036a4465841e26b043761f71789105dd7a1
Vilde Riveland <vilderiveland@gmail.com>
2018-10-23 20:57:13 +0200

    got the static html file to work (ish)

--- src/chatbot/static/script.js
+++ src/chatbot/static/script.js
@@ -1,10 +1,9 @@
 $( document ).ready(function() {
-
+    alert("works");
 });
 
 function ask(){
     var question = document.forms["input"]["question"].value;
-
-
+    $( "#bot" ).html( "<div>doth thou worketh?<br> this isn't working is it?</div>" );
     return false;
 }
