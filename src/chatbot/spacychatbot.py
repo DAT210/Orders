4b0dcdfac5c071dfeff29f4a535a532dd51b1243
Vilde Riveland <vilderiveland@gmail.com>
2018-10-25 14:26:59 +0200

    question sent to python, respose is sent back

--- src/chatbot/spacychatbot.py
+++ src/chatbot/spacychatbot.py
@@ -1,7 +1,7 @@
 import mysql.connector
 import spacy
 import spacy.matcher
-from flask import Flask, render_template
+from flask import Flask, render_template, json, request
 from spacy.matcher import Matcher
 from spacy.matcher import PhraseMatcher
 
@@ -21,32 +21,26 @@ def html():
     return render_template("tester.html")
 
 
+@app.route("/init")
 def init():
     patterns = [nlp(text) for text in openingTimesList]
     matcher.add('openingTimes', None, *patterns)
-    print("init is running")
+    return json.dumps("Hello")
 
 
-def send_response():
-    # send response to js(?)
-    pass
-
-
-def send_question():
-    # send question about whether their question was adequately answered??
-    # wait for answer??
-    pass
-
-
-def respond(sentence):
+@app.route("/respond")
+def respond():
+    global response
+    response = ""
+    sentence = request.args.get("sentence", None)
     prep_sentence(sentence)
     matches = get_matches()
     if not matches:
-        print("no match")
+        not_handled()
     else:
         for match in matches:
             sort(match)
-        send_response()
+    return json.dumps(response)
 
 
 def prep_sentence(sentence):
@@ -55,7 +49,7 @@ def prep_sentence(sentence):
     sent = sentence
     nlpsent = nlp(sent)
     print(nlpsent)
-    # lemmatize to remove plurals
+    # lemmatize to remove some word variations
     global nlp_sent
     nlp_sent = lemmatize(nlpsent)
     print(nlp_sent)
@@ -84,20 +78,34 @@ def get_matches():
 def sort(match):
     print(match['id'])
     match_id = match['id']
-    if match_id == 1715934218517773148:
+    rule_id = nlp.vocab.strings[match_id]
+    print(rule_id)
+    if rule_id == "openingTimes":
         opening_times()
     else:
         not_handled()
 
 
 def not_handled():
+    global response
     print("I can't answer that question, sorry. ")
+    response += "I can't answer that question, sorry. "
 
 
 def opening_times():
     global response
     print("Handle opening times")
-    response += "Your question about opening times cannot be answered yet."
+    response += "Your question about opening times cannot be answered yet. "
+
+
+def test(sentence):
+    prep_sentence(sentence)
+    matches = get_matches()
+    if not matches:
+        print("no match")
+    else:
+        for match in matches:
+            sort(match)
 
 
 if __name__ == "__main__":
--- src/chatbot/static/script.js
+++ src/chatbot/static/script.js
@@ -1,9 +1,27 @@
 $( document ).ready(function() {
     alert("works");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status== 200){
+        }
+    };
+    xhr.open("GET", "/init", true);
+    xhr.send(null);
 });
 
 function ask(){
     var question = document.forms["input"]["question"].value;
+    var url = "/respond?sentence=" + question;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == 4 && xhr.status== 200){
+            var result = xhr.responseText;
+            var obj = JSON.parse(result);
+            alert(obj);
+        }
+    };
+    xhr.open("GET", url, true);
+    xhr.send(null);
     $( "#bot" ).html( "<div>doth thou worketh?<br> this isn't working is it?</div>" );
     return false;
 }
