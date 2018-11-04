af8c6c80ef8b9a5dc9dc19f2bfa4a61029474d86
Ridalor <jacob.ihle@gmail.com>
2018-11-04 19:56:23 +0100

    Added insert commands to .sql file and did a couple of more Unit tests, it now support the json that we are suposed to get from menu, and one of the get request to get courses in an order

--- test/UnitTests.py
+++ test/UnitTests.py
@@ -2,7 +2,16 @@ import unittest
 import requests
 import mysql.connector
 from mysql.connector import errorcode
+import json
+import datetime
 
+CoursesInOrder = '[{"OrderID": 1, "CourseID": 2, "CourseName": "Pepperoni", "Quantity": 3, "Price": 6.35}, ' \
+                     '{"OrderID": 1, "CourseID": 2, "CourseName": "Pepperoni", "Quantity": 3, "Price": 6.35}]'
+
+OrdersFromCustomer = '[{"OrderID": 3, "CustomerID": 1, "OrderTime": "", "PaymentMethod": "Credit",' \
+                     ' "DeliveryMethod": "Pickup", "Price": 9.2, "Payed": ""}, {"OrderID": 3, "CustomerID": 1,' \
+                     ' "OrderTime": "2018-11-04 18:06:07", "PaymentMethod": "Credit", "DeliveryMethod": "Pickup",' \
+                     ' "Price": 9.2, "Payed": ""}]'
 
 try:
     conn = mysql.connector.connect(
@@ -18,27 +27,61 @@ except mysql.connector.Error as err:
 
 
 class MyTest(unittest.TestCase):
-    # Tests getting sent the customer info and if it is properly updated in the db
+    # Tests if sending a json to this url updates the Customer info in the db at the correct OrderID
     def testUpdateCustomer(self):
-        requests.post("http://127.0.0.1:26400/orders/api/Customer", json={"CustomerID": 3, "OrderID": 1})
-        result = CheckCustomerDatabase(3, 1)
-        self.assertEqual(result, 3)
+        requests.post("http://127.0.0.1:80/orders/api/Customer", json={"CustomerID": 1, "OrderID": 1})
+        result = CheckCustomerDatabase(1, 1)
+        self.assertEqual(result, 1)
 
+    # Tests if inserting the json updated the database at the right OrderID
     def testUpdateDeliveryMethod(self):
-        requests.post("http://127.0.0.1:26400/orders/api/Customer", json={"OrderID": 1, "DeliveryMethod": "Car"})
+        requests.post("http://127.0.0.1:80/orders/api/Customer", json={"OrderID": 1, "DeliveryMethod": "Car"})
         result = CheckDeliveryDatabase(1, "Car")
         self.assertEqual(result, "Car")
 
+    # Checks if we get all the courses in an order (this is dependent on a static db)
+    def testCoursesInOrders(self):
+        result = requests.get("http://127.0.0.1:80/orders/api/courses/1")
+        self.assertEqual(result.text, CoursesInOrder)
+
+    # Checks if we get all the orders done by a customer (this is dependen on a static db)
+    def testOrdersDoneByCustomer(self):
+        result = requests.get("http://127.0.0.1:80/orders/api/customerorders/1")
+        wanted = CheckOrdersDoneByCustomer(1)
+        print(result.text)
+        print(wanted)
+        self.assertEqual(result.text, wanted)
+
 
 def CheckCustomerDatabase(CustomerID, OrderID):
-    Query = "SELECT %s FROM Orders WHERE OrderID = %s" % (CustomerID, OrderID)
+    Query = "SELECT %s FROM Orders WHERE OrderID = %s;" % (CustomerID, OrderID)
     cur.execute(Query)
     Customer = cur.fetchall()
     return int(str(Customer)[2])
 
 
 def CheckDeliveryDatabase(OrderID, Method):
-    Query = "SELECT '%s' FROM Orders WHERE OrderID = %s" % (Method, OrderID)
+    Query = "SELECT '%s' FROM Orders WHERE OrderID = %s;" % (Method, OrderID)
     cur.execute(Query)
     DeliveryMethod = cur.fetchall()
     return str(DeliveryMethod)[3:6]
+
+
+def CheckOrdersDoneByCustomer(CustomerID):
+    with open("../src/parsing/Order.json", "r") as f:
+        orderDict = json.load(f)
+    Query = "SELECT * FROM Orders WHERE CustomerID = %s" % CustomerID
+    cur.execute(Query)
+    Orders = cur.fetchall()
+    conn.commit()
+    ListOfOrders = []
+
+    for Order in Orders:
+        for item in Order:
+            key = list(orderDict.keys())[Order.index(item)]
+            if isinstance(item, datetime.datetime):
+                orderDict[key] = str(item)
+            else:
+                orderDict[key] = item
+        ListOfOrders.append(orderDict)
+    return json.dumps(ListOfOrders)
