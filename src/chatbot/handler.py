1e7f3cd9f2f212f32d4dc94dc4c19513aa5a6733
Vilde Riveland <vilderiveland@gmail.com>
2018-11-08 17:03:56 +0100

    restructuring

---
+++ src/chatbot/handler.py
@@ -0,0 +1,79 @@
+from flask import json
+import spacy
+import sqlshit as sql
+import fakereturn as fr
+
+weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+nlp = spacy.load('en_core_web_lg')
+sent = ""
+nlp_sent = ""
+
+
+def not_handled():
+    global response
+    price_comp = nlp("What is the price")
+    availability_comp = nlp("Is a table available")
+    book_comp = nlp("I want to book a table")
+    similarity = {
+        'price': nlp_sent.similarity(price_comp),
+        'availability': nlp_sent.similarity(availability_comp),
+        'book': nlp_sent.similarity(book_comp)
+    }
+    mostLikely = max(similarity, key=similarity.get)
+    sql.notHandled(sent, mostLikely, 0)
+    return "You asked a question I couldn't even sort out. " \
+                "I have been programmed to sort out any intelligent question a customer might have. " \
+                "This means that your question is stupid, you're stupid, and your mom is stupid. <br/>"
+
+
+def opening_times():
+    date = False
+    global response
+    for entity in nlp_sent.ents:
+        if entity.label_ == 'DATE':
+            date = True
+            if entity.text in weekdays:
+                return sql.openingDay(entity.text)
+            else:
+                sql.notHandled(sent, "opening", 1)
+                print(entity.text)
+                return "I don't know the opening times on specific dates, too bad. </br>"
+    if not date:
+        return sql.allOpening()
+
+
+def prices():
+    # TODO find a way to sort out dish names
+    sql.notHandled(sent, "price", 2)
+    info = json.loads(fr.price('pepperoni pizza'))
+    print(info)
+    return "I can't answer questions about prices, " \
+           "as anyone with half a brain could check it out for themselves IN THE MENU </br>"
+
+
+def available():
+    sql.notHandled(sent, "available", 3)
+    for entity in nlp_sent.ents:
+        print(entity.text)
+        if entity.label_ == 'DATE':
+            if entity.text in weekdays:
+                print("weekday")
+                # TODO make weekday into a specific data
+    return "I can't answer questions about table availability, " \
+           "as anyone with half a brain could check it out for themselves if they just followed THIS link </br>"
+
+
+def complaint():
+    sql.complaint(sent, 0)
+    return "Your complaint has been saved, and will be reviewed whenever we feel like it (most likely never). " \
+           "Thank you for not having a life (punk ass bitch). </br>"
+
+
+def location():
+    return "The address for the restaurant is ADDRESS, which you would have known if you'd looked here. </br>"
+
+
+def joke():
+    return "How do you stop a baby from crawling around in circles? Nail it’s other hand to the floor. </br>"
+
--- src/chatbot/spacychatbot.py
+++ src/chatbot/spacychatbot.py
@@ -5,6 +5,7 @@ from spacy.matcher import Matcher
 from spacy.matcher import PhraseMatcher
 import sqlshit as sql
 import fakereturn as fr
+import handler as h
 
 nlp = spacy.load('en_core_web_lg')
 nlp_sent = ""
@@ -14,7 +15,7 @@ priceList = ['price', 'cost', 'how much']
 availabilityList = ['available']
 complaintsList = ['bad', 'horrible', 'terrible', 'dirty', 'slow']
 weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
-locationList = ['where']
+locationList = ['where', 'address']
 matcher = PhraseMatcher(nlp.vocab)
 
 app = Flask(__name__)
@@ -50,7 +51,7 @@ def respond():
     prep_sentence(sentence)
     matches = get_matches()
     if not matches:
-        not_handled()
+        response = h.not_handled()
     else:
         for match in matches:
             sort(match)
@@ -61,10 +62,12 @@ def prep_sentence(sentence):
     # save sentence in case it isn't handled
     global sent
     sent = sentence
+    h.sent = sent
     nlpsent = nlp(sent)
     # lemmatize to remove some word variations
     global nlp_sent
     nlp_sent = lemmatize(nlpsent)
+    h.nlp_sent = nlp_sent
 
 
 def lemmatize(nlpobj):
@@ -88,88 +91,21 @@ def get_matches():
 
 
 def sort(match):
+    global response
     match_id = match['id']
     rule_id = nlp.vocab.strings[match_id]
     if rule_id == "openingTimes":
-        opening_times()
+        response = h.opening_times()
     elif rule_id == "prices":
-        prices()
+        response = h.prices()
     elif rule_id == 'availability':
-        available()
+        response = h.available()
     elif rule_id == 'complaints':
-        complaint()
+        response = h.complaint()
     elif rule_id == 'location':
-        location()
+        response = h.location()
     else:
-        not_handled()
-
-
-def not_handled():
-    global response
-    price_comp = nlp("What is the price")
-    availability_comp = nlp("Is a table available")
-    book_comp = nlp("I want to book a table")
-    similarity = {
-        'price': nlp_sent.similarity(price_comp),
-        'availability': nlp_sent.similarity(availability_comp),
-        'book': nlp_sent.similarity(book_comp)
-    }
-    mostLikely = max(similarity, key=similarity.get)
-    sql.notHandled(sent, mostLikely, 0)
-    response += "You asked a question I couldn't even sort out. " \
-                "I have been programmed to sort out any intelligent question a customer might have. " \
-                "This means that your question is stupid, you're stupid, and your mom is stupid. <br/>"
-
-
-def opening_times():
-    date = False
-    global response
-    for entity in nlp_sent.ents:
-        if entity.label_ == 'DATE':
-            date = True
-            if entity.text in weekdays:
-                response += sql.openingDay(entity.text)
-            else:
-                sql.notHandled(sent, "opening", 1)
-                print(entity.text)
-                response += "I don't know the opening times on specific dates, too bad. </br>"
-    if not date:
-        response = sql.allOpening()
-
-
-def prices():
-    # TODO find a way to sort out dish names
-    sql.notHandled(sent, "price", 2)
-    global response
-    response += "I can't answer questions about prices," \
-                " as anyone with half a brain could check it out for themselves IN THE MENU </br>"
-    info = json.loads(fr.price('pepperoni pizza'))
-    print(info)
-
-
-def available():
-    sql.notHandled(sent, "available", 3)
-    for entity in nlp_sent.ents:
-        print(entity.text)
-        if entity.label_ == 'DATE':
-            if entity.text in weekdays:
-                print("weekday")
-                # TODO make weekday into a specific data
-    global response
-    response += "I can't answer questions about table availability," \
-                " as anyone with half a brain could check it out for themselves if they just followed THIS link </br>"
-
-
-def complaint():
-    global response
-    sql.complaint(sent, 0)
-    response = "Your complaint has been saved, and will be reviewed whenever we feel like it (most likely never). " \
-               "Thank you for not having a life (punk ass bitch). </br>"
-
-
-def location():
-    global response
-    response = "The address for the restaurant is ADDRESS, which you would have known if you'd looked here. </br>"
+        response = h.not_handled()
 
 
 if __name__ == "__main__":
